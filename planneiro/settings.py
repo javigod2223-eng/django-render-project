8d67758c977c04d2a0d4a54b43f2d4a8ce4038e1
Javier <javigod2223@gmail.com>
2025-12-13 19:27:34 -0600

    Activar WhiteNoise para servir archivos estáticos en producción

--- planneiro/settings.py
+++ planneiro/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 ]
 
 # AGREGAR ESTA LÍNEA AQUÍ:
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
